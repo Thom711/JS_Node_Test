9111068821a1bd2d11e7338a07017b75bead6c0f
Thom711 <59656747+Thom711@users.noreply.github.com>
2020-12-16 09:56:36 +0100

    -final

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Hallo Winc Academy studenten!');
+
+// 3. Dit is een grote som
+
+/* 5.
+const multiply = 1230941 * 1823794;
+console.log('1. ', multiply);
+
+
+const divide = 637263 / 54;
+console.log('2. ', divide);
+*/
+
+// 4. Je ziet de comment niet omdat het bedoeld is om je code uit te leggen en of 
+// notities achter te laten, niet om onderdeel te zijn van de runbare code.
+
+// 6. Je ziet geen output meer omdat alles nu een comment is, wat geen
+// onderdeel is van de runbare code, maar commentaar op.
